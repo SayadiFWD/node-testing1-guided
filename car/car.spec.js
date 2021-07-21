c5b082dd68c42d91166628a10fafd90561e71e2d
Gabriel Cabrejas <gabriel@cabrejas.net>
2021-07-21 18:33:40 +0200

    wip

--- car/car.spec.js
+++ car/car.spec.js
@@ -2,6 +2,6 @@ test('it works', () => { // the test
   // empty test is a false positive!
 
   // assertions should be closely related
-  expect(true).not.toBe(true) // one assertion
+  expect(true).not.toBe(false) // one assertion
   expect(true).toBe(true) // another assertion
 })
